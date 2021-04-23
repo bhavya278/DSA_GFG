c3d7875df83124b4a3bdef227030a132566c8d28
Bhavya <bhavyaaggarwal278@gmail.com>
2021-04-23 16:26:03 +0530

    working on arrays

---
+++ Arrays/3.largest_element_in_an_array.cpp
@@ -0,0 +1,61 @@
+//NAIVE APPROACH(O(n^2))
+
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int getLargest(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        bool flag = true;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag == true)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+
+    int arr[] = {5, 8, 20, 10};
+
+    cout << getLargest(arr, 4) << endl;
+
+    return 0;
+}
+*/
+
+//EFFICIENT APPROACH(O(n))
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int getLargest(int arr[], int n)
+{
+    int res = 0;
+    for (int i = 1; i < n; i++)
+        if (arr[i] > arr[res])
+            res = i;
+    return res;
+}
+
+int main()
+{
+
+    int arr[] = {5, 8, 20, 10};
+
+    cout << getLargest(arr, 4) << endl;
+
+    return 0;
+}
---
+++ Arrays/5.Second_Largest_Element.cpp
@@ -0,0 +1,69 @@
+//EFFICIENT APPROACH
+#include <iostream>
+using namespace std;
+int secondLargest(int arr[], int n)
+{
+    int largest = 0, res = -1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > arr[largest])
+        {
+            res = largest;
+            largest = i;
+        }
+        else if (arr[i] != arr[largest])
+        {
+            if (res == -1 || arr[i] > arr[res])
+                res = i;
+        }
+    }
+    return res;
+}
+int main()
+{
+
+    int arr[] = {5, 8, 20, 10};
+
+    cout << secondLargest(arr, 4) << endl;
+
+    return 0;
+}
+
+//NAIVE APPROACH
+/*
+#include <iostream>
+using namespace std;
+int getLargest(int arr[], int n)
+{
+    int largest = 0;
+    for (int i = 1; i < n; i++)
+        if (arr[i] > arr[largest])
+            largest = i;
+    return largest;
+}
+int secondLargest(int arr[], int n)
+{
+    int largest = getLargest(arr, n);
+    int res = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[largest])
+        {
+            if (res == -1)
+                res = i;
+            else if (arr[i] > arr[res])
+                res = i;
+        }
+    }
+    return res;
+}
+int main()
+{
+
+    int arr[] = {5, 8, 20, 10};
+
+    cout << secondLargest(arr, 4) << endl;
+
+    return 0;
+}
+*/
