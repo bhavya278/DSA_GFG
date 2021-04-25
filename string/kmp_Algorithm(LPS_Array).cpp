8a78153b95439006dc515ccc1f4f9307598c72cf
Bhavya <bhavyaaggarwal278@gmail.com>
2021-04-25 05:41:27 +0530

    updating string

---
+++ string/kmp_Algorithm(LPS_Array).cpp
@@ -0,0 +1,67 @@
+//EFFICIENT APPROACH(O(N))
+/*
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+void fillLPS(string str, int *lps){
+    int n=str.length(),len=0;
+    lps[0]=0;
+    int i=1;
+    while(i<n){
+        if(str[i]==str[len])
+        {len++;lps[i]=len;i++;}
+        else
+        {if(len==0){lps[i]=0;i++;}
+            else{len=lps[len-1];}
+        }
+    }
+}
+ 
+int main() 
+{ 
+    string txt = "abacabad";int lps[txt.length()];
+    fillLPS(txt,lps);
+    for(int i=0;i<txt.length();i++){
+        cout<<lps[i]<<" ";
+    }
+    
+    return 0; 
+} 
+*/
+
+//NAIVE APPROACH O(N^3)
+/*
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int longPropPreSuff(string str, int n){
+    for(int len=n-1;len>0;len--){
+        bool flag=true;
+        for(int i=0;i<len;i++)
+            if(str[i]!=str[n-len+i])
+                flag=false;
+                
+        if(flag==true)
+            return len;
+    }
+    return 0;
+}
+
+void fillLPS(string str, int *lps){
+    for(int i=0;i<str.length();i++){
+        lps[i]=longPropPreSuff(str,i+1);
+    }
+}
+ 
+int main() 
+{ 
+    string txt = "abacabad";int lps[txt.length()];
+    fillLPS(txt,lps);
+    for(int i=0;i<txt.length();i++){
+        cout<<lps[i]<<" ";
+    }
+    
+    return 0; 
+} 
+*/
