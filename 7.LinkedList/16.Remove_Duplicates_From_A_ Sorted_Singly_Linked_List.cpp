afc0d33f25a3459613f5643a63c8d3399fbbe3cb
Bhavya <bhavyaaggarwal278@gmail.com>
2021-04-29 05:35:00 +0530

    finish linked list folder

---
+++ 7.LinkedList/16.Remove_Duplicates_From_A_ Sorted_Singly_Linked_List.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Node remDup(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL && curr->next != NULL)
+    {
+        if ((curr->data) == (curr->next->data))
+        {
+            Node *temp = curr->next;
+            curr->next = curr->next->next;
+            delete (temp);
+        }
+        else
+        {
+            curr = curr->next;
+        }
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(20);
+    head->next->next->next = new Node(30);
+    head->next->next->next->next = new Node(30);
+    head->next->next->next->next->next = new Node(30);
+    printlist(head);
+    remDup(head);
+    printlist(head);
+    return 0;
+}
